03bfd8b518283cb3cf33d1d698da72ff1ea265bc
Ed <djaziz12393@gmail.com>
2018-04-10 16:21:25 +0300

    Работа в отсоединенном режиме, fill and update

---
+++ DataSet(ADO.Net)/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace DataSet_ADO.Net_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtBxQuery = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnFill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(647, 37);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtBxQuery
+            // 
+            this.txtBxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBxQuery.Location = new System.Drawing.Point(56, 11);
+            this.txtBxQuery.Name = "txtBxQuery";
+            this.txtBxQuery.Size = new System.Drawing.Size(628, 20);
+            this.txtBxQuery.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(729, 287);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(13, 37);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(75, 23);
+            this.btnFill.TabIndex = 3;
+            this.btnFill.Text = "Fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 354);
+            this.Controls.Add(this.btnFill);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBxQuery);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtBxQuery;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnFill;
+    }
+}
+
---
+++ DataSet(ADO.Net)/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataSet_ADO.Net_
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlDataAdapter dtAdptr=null;
+        DataSet dtSt = null;
+        SqlCommandBuilder SqlcmdBldr = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = txtBxQuery.Text;
+                dtAdptr = new SqlDataAdapter(query, conn);
+                SqlcmdBldr = new SqlCommandBuilder(dtAdptr);
+                dtSt = new DataSet();
+                dtAdptr.Fill(dtSt, "mybook");
+                dataGridView1.DataSource = dtSt.Tables["mybook"];
+            }
+            catch { throw new Exception(); }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+           
+            dtAdptr.Update(dtSt,"mybook");
+
+        }
+    }
+}
